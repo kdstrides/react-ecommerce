41af4e0bb959164109ca45d6b08cb6bdc5dc2236
Dipesh Parmar <dipeshbeckham@gmail.com>
2020-05-16 19:53:05 +0530

    Cart icon and dropdown added in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/original.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -19,6 +21,8 @@ const Header = ({ currentUser }) => (
           <div className="option" onClick={() => auth.signOut()}> SIGN OUT</div>
         : <Link className="option" to="/signin">SIGNIN</Link>
       }
+      <CartIcon />
+      <CartDropdown />
     </div>
   </div>
 );
