3262eee52d494bef5b55add8030c27c0ab05c1cb
Dipesh Parmar <dipeshbeckham@gmail.com>
2020-05-20 21:24:21 +0530

    Shop sub pages corrections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import { Route } from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
 
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route component={CollectionOverview} exact path={`${match.path}`} />
+    <Route component={CollectionPage} path={`${match.path}/:collectionId`} />
+  </div>
+);
 
 export default ShopPage;
